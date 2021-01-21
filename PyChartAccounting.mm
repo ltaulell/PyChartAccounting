2014e62911417ea352a8f1b944e8d5525ce4a447
Loïs Taulelle <lois.taulelle@ens-lyon.fr>
2021-01-21 11:45:12 +0100

    add more model thingies

---
+++ PyChartAccounting.mm
@@ -0,0 +1,106 @@
+<map version="freeplane 1.7.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="PyChartAccounting" FOLDED="false" ID="ID_1847389613" CREATED="1611222274847" MODIFIED="1611222529487" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false" show_note_icons="true"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="4" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="besoins" POSITION="right" ID="ID_879044232" CREATED="1611222290374" MODIFIED="1611222537746">
+<edge COLOR="#ff0000"/>
+<font SIZE="12" BOLD="true"/>
+<node TEXT="analyser le fichier d&apos;accounting &quot;&#xe0; froid&quot;" ID="ID_1458569467" CREATED="1611222295206" MODIFIED="1611222309578"/>
+<node TEXT="permettre une visualisation graphique des r&#xe9;sultats" ID="ID_1522155414" CREATED="1611222310694" MODIFIED="1611222327130"/>
+<node TEXT="ouvrir aux utilisateurs du cluster (intranet)" ID="ID_434819635" CREATED="1611222329271" MODIFIED="1611223609154"/>
+<node TEXT="https://rdlab.cs.upc.edu/s-gae/" ID="ID_1762824682" CREATED="1611222433690" MODIFIED="1611222438299"/>
+</node>
+<node TEXT="fichier d&apos;accounting" POSITION="left" ID="ID_75483259" CREATED="1611222349223" MODIFIED="1611222393856">
+<edge COLOR="#0000ff"/>
+<richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      voir :
+    </p>
+    <p>
+      https://github.com/ltaulell/PyChartAccounting/blob/master/SGE_accounting_file_format.rst
+    </p>
+    <p>
+      https://github.com/ltaulell/PyChartAccounting/blob/master/analysis.rst
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node TEXT="contraintes" POSITION="right" ID="ID_1446962237" CREATED="1611222476841" MODIFIED="1611222544594" HGAP_QUANTITY="16.249999932944775 pt" VSHIFT_QUANTITY="29.999999105930357 pt">
+<edge COLOR="#ff00ff"/>
+<font SIZE="12" BOLD="true"/>
+<node TEXT="python3" ID="ID_1776342741" CREATED="1611222481689" MODIFIED="1611222486829"/>
+<node TEXT="base de donn&#xe9;es ?" ID="ID_1226836252" CREATED="1611222489401" MODIFIED="1611222494252"/>
+<node TEXT="client web pour la visualisation" ID="ID_783243255" CREATED="1611222497290" MODIFIED="1611222511581"/>
+</node>
+</node>
+</map>
