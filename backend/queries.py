07ef36ee8570a4dbaaae503f70012257e374b1df
Loïs Taulelle <lois.taulelle@ens-lyon.fr>
2021-03-11 16:06:23 +0100

    proper queries, for HCERES and annual statistiques purposes

--- backend/queries.py
+++ backend/queries.py
@@ -10,55 +10,60 @@
 """
 perform some queries on database
 
--- # https://www.epochconverter.com/
--- epoch (2010-01-01) 1262304000
--- epoch (2010-12-31) 1293753600
--- epoch (2011-12-31) 1325289600
--- epoch (2012-12-31) 1356912000
--- epoch (2013-12-31) 1388448000
--- epoch (2014-12-31) 1419984000
--- epoch (2015-12-31) 1451520000
--- epoch (2016-12-31) 1483142400
--- epoch (2017-12-31) 1514678400
--- epoch (2018-12-31) 1546214400
--- epoch (2019-12-31) 1577750400
--- epoch (2020-12-31) 1609372800
--- epoch (2021-12-31) 1640908800
+# https://towardsdatascience.com/reshape-pandas-dataframe-with-pivot-table-in-python-tutorial-and-visualization-2248c2012a31
+# https://stackoverflow.com/questions/13784192/creating-an-empty-pandas-dataframe-then-filling-it/56746204#56746204
+
+# https://www.epochconverter.com/
 
 """
 
-# import argparse
+import argparse
 import logging
-import psycopg2
 import datetime
-# from pprint import pprint
+import pandas as pd
+from pprint import pprint
+import psycopg2
 
 import config
 
 log = logging.getLogger()
 stream_handler = logging.StreamHandler()
 log.addHandler(stream_handler)
-# log.setLevel('DEBUG')
 
-YEARS = [(1262304000, 1293753600),
-         (1293753600, 1325289600),
-         (1325289600, 1356912000),
-         (1356912000, 1388448000),
-         (1388448000, 1419984000),
-         (1419984000, 1451520000),
-         (1451520000, 1483142400),
-         (1483142400, 1514678400)
+# TODO/FIXME
+# make it a function, from first day of the year to first day of the next year
+YEARS = [(1262304000, 1293840000),
+         (1293840000, 1325376000),
+         (1325376000, 1356998400),
+         (1356998400, 1388534400),
+         (1388534400, 1420070400),
+         (1420070400, 1451606400),
+         (1451606400, 1483228800),
+         (1483228800, 1514764800)
          ]
 """
-         (1514678400, 1546214400),
-         (1546214400, 1577750400),
-         (1577750400, 1609372800),
-         (1609372800, 1640908800)
+         (1514764800, 1546300800),
+         (1546214400, 1577836800),
+         (1577836800, 1609459200),
+         (1609459200, 1640995200)
          ]
 """
 
 
-def execute_sql(connexion, commande, payload, commit=False):
+def get_args(helper=False):
+    """ read parser and return args (as args namespace),
+        if helper=True, show usage() or help()
+    """
+    parser = argparse.ArgumentParser(description='query accounting database')
+    parser.add_argument('-d', '--debug', action='store_true', help='toggle debug ON')
+
+    if helper:
+        return parser.print_usage()
+    else:
+        return parser.parse_args()
+
+
+def execute_sql(connexion, commande, payload, commit=False, fetchall=False):
     """ execute commande, always return id
     SQL inserts MUST returning ids, else fetchone() will fail """
     try:
@@ -67,8 +72,10 @@ def execute_sql(connexion, commande, payload, commit=False):
             if commit:
                 connexion.commit()
             log.debug('status: {}'.format(cursor.statusmessage))
-            # return cursor.fetchone()
-            return cursor.fetchall()
+            if fetchall:
+                return cursor.fetchall()
+            else:
+                return cursor.fetchone()
 
     except psycopg2.errors.StringDataRightTruncation as e:
         # if job_name or project is too long, ignore job, there's a problem.
@@ -88,7 +95,6 @@ def prepare_select(conn, payload):
     """ """
     log.debug('payload: {}'.format(payload))
 
-    # start, end = payload
     # sql_str = ''.join(['SELECT ', id_name, ' FROM ', table, ' WHERE ', name, ' LIKE (%s);'])
     sql_str = """ SELECT groupes.group_name, sum(job_.cpu) AS sum_cpu, count(job_.id_job_) AS nb_job
                   FROM job_, groupes
@@ -98,29 +104,54 @@ def prepare_select(conn, payload):
                   GROUP BY groupes.group_name
                   ORDER BY sum_cpu DESC; """
 
-    result = execute_sql(conn, sql_str, payload)
+    result = execute_sql(conn, sql_str, payload, fetchall=True)
     log.debug('select: {}'.format(result))
     return result
 
 
 if __name__ == '__main__':
     """ """
+    args = get_args()
+
+    if args.debug:
+        log.setLevel('DEBUG')
+        log.debug(get_args(helper=True))
+
     # prepare la config locale pgsql
     param_conn_db = config.parserIni(filename='infodb.ini', section='postgresql')
     log.debug(param_conn_db)
 
+    data = []
+
     try:
         with psycopg2.connect(**param_conn_db) as conn:
             log.debug(conn)
-            for year in YEARS:
-                start, end = year
-                # print(datetime.datetime.fromtimestamp(start).strftime('%Y-%m-%d'))
-                # print(datetime.datetime.fromtimestamp(end).strftime('%Y-%m-%d'))
-                print('année: {}'.format(datetime.datetime.fromtimestamp(end).strftime('%Y')))
-                resultat = prepare_select(conn, year)
-                # pprint(resultat)
-                for groupe, secondes, nb in resultat:
-                    print('{};{};{:.0f}'.format(groupe, nb, secondes / 3600))
+
+            for epoch in YEARS:
+                start, end = epoch
+                year = datetime.datetime.fromtimestamp(start).strftime('%Y')
+                print('année: {}'.format(year))
+                resultats = prepare_select(conn, epoch)
+                log.debug('resultats: {}'.format(resultats))
+
+                for (groupe, secondes, jobs) in resultats:
+                    heures = int(secondes / 3600)
+                    log.debug('resultat: {}, {}, {}'.format(groupe, jobs, heures))
+
+                    data.append([groupe, year, 'time', heures])
+                    data.append([groupe, year, 'jobs', jobs])
+
+            # pprint(data)
+            df_long = pd.DataFrame(data, columns=['groupes', 'year', 'type', 'results'])
 
     except psycopg2.Error as e:
         log.debug(e.diag.message_primary)
+
+    log.debug(df_long.columns)
+    # pprint(df_long.describe())
+    df_wide = df_long.pivot_table(index='groupes',
+                                  columns=['year', 'type'],
+                                  values='results',
+                                  fill_value=0)
+    pprint(df_wide)
+    df_wide.to_csv('accounting_groups_.csv', encoding='utf-8')
