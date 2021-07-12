2495b9c21db8a13ae471855c2a901d55816e6373
zDeltas <dleborgne12@gmail.com>
2021-07-12 16:42:21 +0200

    Imported old code + added flash method

---
+++ frontend/app/static/js/charts-custom.js
@@ -0,0 +1,267 @@
+/**
+ * Copyright (c) 2021
+ *
+ * long description for the file
+ *
+ * @summary file which contains the functions allowing the creation of charts
+ * @author Damien LE BORGNE
+ *
+ * Created at     : 2021-03-07
+ * Last modified  : 2021-03-11
+ */
+
+var legends = {
+    display: true,
+    position: 'bottom',
+    labels: {
+        generateLabels: function(chart) {
+            var data = chart.data;
+            if (data.labels.length && data.datasets.length) {
+                return data.labels.map(function(label, i) {
+                    var meta = chart.getDatasetMeta(0);
+                    var datasets = data.datasets[0];
+                    var arc = meta.data[i];
+                    var custom = arc && arc.custom || {};
+                    var getValueAtIndexOrDefault = Chart.helpers.getValueAtIndexOrDefault;
+                    var arcOpts = chart.options.elements.arc;
+                    var fill = custom.backgroundColor ? custom.backgroundColor : getValueAtIndexOrDefault(datasets.backgroundColor, i, arcOpts.backgroundColor);
+                    var stroke = custom.borderColor ? custom.borderColor : getValueAtIndexOrDefault(datasets.borderColor, i, arcOpts.borderColor);
+                    var bw = custom.borderWidth ? custom.borderWidth : getValueAtIndexOrDefault(datasets.borderWidth, i, arcOpts.borderWidth);
+
+                    var value = chart.config.data.datasets[arc._datasetIndex].data[arc._index];
+
+                    return {
+                        text: label + " : ~" + Math.round(value*1000)/1000,
+                        fillStyle: fill,
+                        strokeStyle: stroke,
+                        lineWidth: bw,
+                        index: i
+                    };
+                });
+            } else {
+                return [];
+            }
+        }
+    }
+}
+
+
+function drawPieChart(IdChart, labelsName, dataList){
+
+    var brandPrimary = 'rgba(179, 50, 155, 1)';
+
+    var PieChartId    = $('#'+IdChart);
+
+    var pieChart = new Chart.Doughnut(PieChartId, {
+        data: {
+            labels: labelsName,
+            datasets: [
+                {
+                    data: dataList,
+                    backgroundColor: [
+                        brandPrimary,
+                        "rgba(75,192,192,1)",
+                        "#56ffce",
+                        "rgba(250, 15, 15, 1)"
+                    ],
+                }]
+            },
+        options: {
+            responsive: true,
+            tooltips: {
+            callbacks: {
+                label: function(tooltipItem, data) {
+                var dataset = data.datasets[tooltipItem.datasetIndex];
+                var total = dataset.data.reduce(function(previousValue, currentValue, currentIndex, array) {
+                return previousValue + currentValue;
+            });
+                var currentValue = dataset.data[tooltipItem.index];
+                var percentage = Math.floor(((currentValue/total) * 100));         
+                return currentValue +" ≃ "+ percentage + "%";
+                    }
+                },
+                title: {
+                    display: true,
+                    text: 'Custom Chart Title'
+                }
+            },
+            legend: legends
+        }
+    });
+
+}
+
+
+
+function drawLineChart(IdChart){
+
+    var linePrimary = 'rgba(51, 179, 90, 1)';
+    var LineChartId  = $('#'+IdChart);
+
+    var lineChart = new Chart.Line(LineChartId, {
+        data: {
+          labels: ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"],
+          datasets: [{
+            label: "Utilisateur",
+                    fill: true,
+                    lineTension: 0.3,
+                    backgroundColor: "rgba(51, 179, 90, 0.38)",
+                    borderColor: linePrimary,
+                    borderCapStyle: 'butt',
+                    borderDash: [],
+                    borderDashOffset: 0.0,
+                    borderJoinStyle: 'miter',
+                    borderWidth: 1,
+                    pointBorderColor: linePrimary,
+                    pointBackgroundColor: "#fff",
+                    pointBorderWidth: 1,
+                    pointHoverRadius: 5,
+                    pointHoverBackgroundColor: linePrimary,
+                    pointHoverBorderColor: "rgba(220,220,220,1)",
+                    pointHoverBorderWidth: 2,
+                    pointRadius: 1,
+                    pointHitRadius: 10,
+                    data: [10, 20, 30, 40, 50, 60, 60, 50, 40, 30, 20, 10],
+                    spanGaps: false
+          }]
+        },
+        options: {
+          responsive: true,
+          tooltips: {
+            mode: 'index',
+            intersect: true
+          }
+        }
+      });
+}
+
+function drawBarChart(IdChart, labels, values, compare){
+
+    const chartLegendSelector = "[data-results-chart-legends]";
+    let chartLegendEL;
+    chartLegendEL = document.querySelector(chartLegendSelector);
+
+    var UserPrimary = 'rgba(51, 179, 90, 1)';
+    var AllPrimary = 'rgba(203, 203, 203, 1)';
+    var BarChartId  = $('#'+IdChart);
+
+    var barChart = new Chart.Bar(BarChartId, {
+        data: {
+            labels: labels,
+            datasets: [
+                {
+                    backgroundColor: UserPrimary,
+                    borderColor: UserPrimary,
+
+                    borderWidth: 1,
+                    data: values,
+                },
+            ]
+        }, 
+        options: {
+            legend: legends
+        }
+    });
+
+}
+
+
+
+function drawBarStacked(IdChart, labels, values){
+    if(values[0] < values[1]){
+        pgValue = values[1];
+        ppValue = values[0];
+        pgLabel = labels[1];
+        ppLabel = labels[0];
+    }
+    var UserPrimary = 'rgba(51, 179, 90, 1)';
+    var AllPrimary = 'rgba(203, 203, 203, 1)';
+    var BarChartId  = $('#'+IdChart);
+
+    var barChart = new Chart.Bar(BarChartId, {
+        data: {
+            datasets: [
+                {
+                    label: pgLabel,
+                    backgroundColor: UserPrimary,
+                    borderColor: UserPrimary,
+
+                    borderWidth: 1,
+                    data: [pgValue],
+                },
+                {
+                    label: ppLabel,
+                    backgroundColor: AllPrimary,
+
+                    borderColor: AllPrimary,
+
+                    borderWidth: 1,
+                    data: [ppValue],
+                }
+            ]
+        },
+        options : {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true,
+                        min: 0
+                    },
+                    stacked: true
+                }], 
+                xAxes: [{ stacked: true }]
+            },
+
+        }
+    });
+
+}
+
+
+function drawBarCompareChart(IdChart, labels, values){
+    if(values[0] < values[1]){
+        pgValue = values[1];
+        ppValue = values[0];
+        pgLabel = labels[1];
+        ppLabel = labels[0];
+    }
+    var UserPrimary = 'rgba(51, 179, 90, 1)';
+    var AllPrimary = 'rgba(203, 203, 203, 1)';
+    var BarChartId  = $('#'+IdChart);
+
+    var barChart = new Chart.Bar(BarChartId, {
+        data: {
+            datasets: [
+                {
+                    label: pgLabel,
+                    backgroundColor: UserPrimary,
+                    borderColor: UserPrimary,
+
+                    borderWidth: 1,
+                    data: [pgValue],
+                },
+                {
+                    label: ppLabel,
+                    backgroundColor: AllPrimary,
+
+                    borderColor: AllPrimary,
+
+                    borderWidth: 1,
+                    data: [ppValue],
+                }
+            ]
+        },
+        options : {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true,
+                        min: 0
+                    }
+                }]
+            },
+
+        }
+    });
+
+}
---
+++ frontend/app/static/js/front.js
@@ -0,0 +1,106 @@
+/**
+ * @author Damien LE BORGNE
+ */
+
+function loadGroups(name){
+
+    $.getJSON('/output/'+name, function(data, status, xhr){
+
+        if(data["groupes"].length > 1){
+            var options = '<option value="Tout" selected>Tout</option>';
+        }else{
+            var select = "selected";
+            var options = '<option value="Tout">Tout</option>';
+        }
+        
+        for (var i = 0; i < data["groupes"].length; i++ ) {
+            options += '<option value="' + data["groupes"][i]+ '" '+select+'>' + data["groupes"][i] + '</option>';
+            select = "";
+        }
+        // console.log(data["groupes"]);
+        $("#groups").html(options);  
+    });
+}
+
+function loadLists(){
+    
+    var listUsers = [];
+
+//  Ajouter tout les utilisateurs de la bdd dans un array retourner la liste sous format json dans /users (python), Recuperer la liste et le push dans listUsers (js)
+    $.getJSON('/users', function(data){
+     for (var i = 0; i < data["users"].length; i++ ) {
+        listUsers.push(data["users"][i]);
+     }
+    });
+
+//  Charger les groupes dans un select
+    $.getJSON('/who', function(name){
+        loadGroups(name["user"]);
+    });
+
+//  Charger les queues dans un select
+    $.getJSON('/queues', function(data, status, xhr){
+        var options = '<option value="Tout" selected>Tout</option>';
+        for (var i = 0; i < data["queues"].length; i++ ) {
+            options += '<option value="' + data["queues"][i]+ '">' + data["queues"][i] + '</option>';
+            select = "";
+        }
+        // console.log(data["groupes"]);
+        $("#queue").html(options);  
+    });
+
+//  Charger les clusters dans un select
+    $.getJSON('/clusters', function(data, status, xhr){
+        let select = "selected";
+        let options = '<option value="Tout">Tout</option>';
+        for (var i = 0; i < data["clusters"].length; i++ ) {
+            options += '<option value="' + data["clusters"][i]+ '" '+select+'>' + data["clusters"][i] + '</option>';
+            select = "";
+        }
+        // console.log(data["groupes"]);
+        $("#cluster").html(options);  
+    });
+
+// Autocompletion
+   $( "#users" ).autocomplete({
+    source: function( request, response ) {
+            var matcher = new RegExp( "^" + $.ui.autocomplete.escapeRegex( request.term ), "i" );
+            response( $.grep( listUsers, function( item ){
+                return matcher.test( item );
+            }) );
+        }
+    });
+};
+
+$(document).ready(function(){
+
+    loadLists();
+
+//  Recuperer l'id de l'utilisateur dans /who puis pour chaque Keyup, retourner l'input /output/id Flask retournera une reponse
+    $.getJSON('/who', function(name){
+        $("#users").keyup(function(){
+            var text = $(this).val();
+            $.ajax({
+              url: "/output/"+name["user"],
+              type: "get",
+              data: {outData: text},
+              success: function(response) {
+                loadGroups(name["user"]);
+              },
+              error: function(err) {
+                console.log(err)
+              }
+            });
+        });
+    });
+   
+//  Eviter la frappe entrée lorsque l'utilisateur entre un nom (eviter de valider le formulaire)
+    $(window).keydown(function(event){
+        if(event.keyCode == 13) {
+          event.preventDefault();
+          return false;
+        }
+      });
+}); 
+
+
