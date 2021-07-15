b817e1cf246e7aacad675c10590958f02b7f1120
zDeltas <dleborgne12@gmail.com>
2021-07-15 13:29:49 +0200

    Get the handler

--- frontend/app/errors_handlers.py
+++ frontend/app/errors_handlers.py
@@ -2,13 +2,12 @@ import warnings
 from app import app
 from flask import redirect, url_for, flash
 
-
 @app.errorhandler(404)
-def not_found():
-    flash("Erreur 404 - La page demandée est inconnu", category="warning")
+def not_found(e):
+    flash("Erreur 404 - La page demandée n'existe pas", category="warning")
     return redirect(url_for("index"))
 
 @app.errorhandler(400)
-def not_found():
+def not_found(e):
     flash("Erreur 400 - Erreur dans la requête", category="warning")
     return redirect(url_for("index"))
