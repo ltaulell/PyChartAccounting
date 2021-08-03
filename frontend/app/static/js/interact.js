095ed037bfe2db24e4bf9541564da27aa7e45a29
zDeltas <dleborgne12@gmail.com>
2021-08-03 19:06:39 +0200

    Changed html2canvas for dom-to-image

--- frontend/app/static/js/interact.js
+++ frontend/app/static/js/interact.js
@@ -1,14 +1,11 @@
 $('button.toJpg').on('click', function(e){
     e.preventDefault();
-    console.log(this.id);
-    window.scrollTo(0,0);  
-    html2canvas($("#"+this.id)[0], {scrollY: -window.scrollY}).then((canvas) => {
-        console.log("done ... ");
-        document.body.appendChild(canvas);
 
-        canvas.toBlob(function(blob) {
-            saveAs(blob, "img.jpg"); 
-        });
+    var node = document.getElementById(this.id);
+    var name = this.id.replace("-img", "")
+   
+    domtoimage.toBlob(node).then(function (blob) {
+        window.saveAs(blob, name+'.png');
     });
 
 });
