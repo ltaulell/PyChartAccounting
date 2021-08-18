f58c5e90e8d8cca0fbc567257af1ab3828fe7cdf
damien LE BORGNE <dleborgne12@gmail.com>
2021-08-18 14:35:35 +0200

    Open navbar recovery

---
+++ frontend/app/static/js/formSetup.js
@@ -0,0 +1,25 @@
+function formsetup(data) {
+
+    if(data.hasOwnProperty('queue')){
+        document.getElementById("queue").value = data["queue"];
+        document.getElementById('queueSearch').click();
+    }
+
+    if(data.hasOwnProperty('cluster')){
+        document.getElementById("cluster").value = data["cluster"];
+        document.getElementById('clusterSearch').click();
+    }
+
+    if(data.hasOwnProperty('Année')){
+        document.getElementById('anneeSearch').click();
+    }
+
+    if(data.hasOwnProperty('Debut')){
+        document.getElementById('periodeSearch').click();
+    }
+
+    
+
+    
+}
+
